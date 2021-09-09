e364afa533b2a2804525e4573bef312efcb31300
zubae <zubae@live.in>
2021-09-09 18:11:21 +0530

    init, installed firebase

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 const App = () => {
-  const test = 'example_string'
-
   return (
     <div>
-      <Helmet>
-        <meta property='og:tittle' content={`Test Title ${test}`} />
-      </Helmet>
-      <h1>Hello World</h1>
+      <h1>PC-Ecard</h1>
     </div>
   )
 }
