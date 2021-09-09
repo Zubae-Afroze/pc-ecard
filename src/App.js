f36264e5ea732e74d60f7f9c05cf22db702aa9b4
zubae <zubae@live.in>
2021-09-09 18:06:06 +0530

    Inital cleanup, React helmet, git remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Helmet } from 'react-helmet'
 
-function App() {
+const App = () => {
+  const test = 'example_string'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <meta property='og:tittle' content={`Test Title ${test}`} />
+      </Helmet>
+      <h1>Hello World</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
